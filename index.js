0be67fd6a9cd7a99f0b73c829329791f6834aebd
codeswitchstudio <toolbarista@gmail.com>
2024-02-10 14:41:01 +0000

    adding manager to index

--- index.js
+++ index.js
@@ -15,31 +15,71 @@ const render = require("./src/page-template.js");
 const teamMembers = [];
 
 // Write code in index.js that uses inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
+
+
+
 // When a user starts the application then they are prompted to enter the team manager’s:
 // Name
 // Employee ID
 // Email address
 // Office number
+
 function promptManager() {
     inquirer
-      .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is your project about?',
-    },
-    {
-        type: 'input',
-        name: 'userStory',
-        message: 'Please input your user story:',
-    }
+        .prompt([    /* Pass your questions in here */
+        { 
+            type: 'input', 
+            name: 'name',
+            message: 'Please enter team manager\'s name.',
+            // validate:      
+        } ,
+        { 
+            type: 'input', 
+            name: 'id',
+            message: 'Please enter team manager\'s ID number.',
+            // validate:      
+        } ,
+        { 
+            type: 'input', 
+            name: 'email',
+            message: 'Please enter team manager\'s email address',
+            // validate:      
+        } ,
+        { 
+            type: 'input', 
+            name: 'officeNumber',
+            message: 'Please enter team manager\'s office number',
+            // validate:      
+        }         
+        ])
+        .then((answers) => {
+            // Add input to array
+            const  manager = new Manager(
+                answers.name, 
+                answers.id, 
+                answers.email, 
+                answers.officeNumber
+            );
+            teamMembers.push(manager);
+            console.log(`Adding the manager to the team...`);
+            // Call function to add more members or build the team
+            addTeamMember();
+        });    
 
-])};
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+            } else {
+            // Something else went wrong
+            }
+        });
+        // When a user enters those requirements then the user is presented with a menu with the option to:
+        // Add an engineer
+        // Add an intern
+        // Finish building the team
+        
+}
 
 
 
@@ -54,10 +94,7 @@ function promptManager() {
 
 
 
-// When a user enters those requirements then the user is presented with a menu with the option to:
-// Add an engineer
-// Add an intern
-// Finish building the team
+
 
 
 // When a user selects the engineer option then a user is prompted to enter the following and then the user is taken back to the menu:
@@ -78,6 +115,11 @@ function promptManager() {
 // School
 
 
+
+
+function addTeamMember() {
+
+}
 
 // When a user decides to finish building their team then they exit the application, and the HTML is generated.
 
